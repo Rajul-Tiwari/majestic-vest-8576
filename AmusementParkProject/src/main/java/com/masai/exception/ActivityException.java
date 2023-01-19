00856214d8dcf627a4c8ed2f204d7a1b05831fd3
rakeshrauniyar12 <rakeshrauniyar12>
2023-01-19 23:29:46 +0530

    added

---
+++ AmusementParkProject/src/main/java/com/masai/exception/ActivityException.java
@@ -0,0 +1,8 @@
+package com.masai.exception;
+
+public class ActivityException extends Exception{
+    public ActivityException() {};
+    public ActivityException(String message) {
+    	super(message);
+    }
+}
