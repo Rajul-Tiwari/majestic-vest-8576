1c507e61f58ae3b3d9111021e05e52667b151334
rakeshrauniyar12 <rakeshrauniyar12>
2023-01-20 00:11:20 +0530

    added

--- AmusementParkProject/src/main/java/com/masai/services/AdminServiceImpl.java
+++ AmusementParkProject/src/main/java/com/masai/services/AdminServiceImpl.java
@@ -2,6 +2,7 @@ package com.masai.services;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -9,12 +10,14 @@ import com.masai.exception.ActivityException;
 import com.masai.exception.AdminException;
 import com.masai.model.Activity;
 import com.masai.model.Admin;
+import com.masai.repository.ActivityRepo;
 import com.masai.repository.AdminRepo;
 
 public class AdminServiceImpl implements AdminService{
            @Autowired
 	private AdminRepo adminRepo;
-	
+	@Autowired
+	private ActivityRepo activityRepo;
 	
 	@Override
 	public Admin insertAdmin(Admin admin) {
@@ -27,18 +30,35 @@ public class AdminServiceImpl implements AdminService{
 
 	@Override
 	public Admin updateAdmin(Admin admin) throws AdminException {
-		             
-		
+		                Optional<Admin> opt= adminRepo.findById(admin.getAdminID());
+		                
+		                if(opt.isPresent()) {
+		                	       Admin updateAdmin=   adminRepo.save(admin);
+		                	       return updateAdmin;
+		                } else {
+		                	throw new AdminException("Admin does not exits");
+		                }
 	}
 
 	@Override
 	public Admin deleteAdmin(Integer adminId) throws AdminException {
-		// TODO Auto-generated method stub
-		return null;
+		              Optional<Admin> opt= adminRepo.findById(adminId);
+		              
+		              if(opt.isPresent()) {
+		            	         adminRepo.delete(opt.get());
+		            	         return opt.get();
+		              } else {
+		            	  throw new AdminException("Admin does not found with adminId:"+adminId);
+		              }
 	}
 
 	@Override
 	public List<Activity> getAllActivities(Integer cutomerId) throws ActivityException {
+		
+		                          
+		
+		
+		
 		// TODO Auto-generated method stub
 		return null;
 	}
