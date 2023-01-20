93ac82d1de8394106ed829d7326677c42ee7cd34
rakeshrauniyar12 <rakeshrauniyar12>
2023-01-21 01:10:32 +0530

    added

--- AmusementParkProject/src/main/java/com/masai/model/Admin.java
+++ AmusementParkProject/src/main/java/com/masai/model/Admin.java
@@ -1,10 +1,5 @@
 package com.masai.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,29 +8,49 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Getter
-@Setter
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Admin {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+	
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer adminID;
-    @NotNull
-    private String User_Name;
+	
+	@NotNull
+	@Size(min=4,max=12,message = "Password should has minimum 4 to 12 characters")
+    private String user_Name;
 
-    @Size(min=4,max=12,message = "Password should has minimum 4 to 12 characters")
-    private String password;
 
-    @NotNull
+	@Size(min=4,max=12,message = "Password should has minimum 4 to 12 characters")
+	private String password;
+
+	@NotNull
     private String address;
 
     @Size(min=10, message ="Mobile Number should be of 10 digits!")
-    private String mobileNumber;
+	private String mobileNumber;
 
-    @Email
+	@Email
     private String email;
+	
+	
+	
+	
+	
+	
+	
+	
 
+    
+    
 
 }
